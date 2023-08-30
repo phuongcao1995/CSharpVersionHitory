618c1088974342de090b614a25f42e4dedb18d23
Phuong Cao Ngoc <phuong.cao.ngoc@dmchosting.ca>
2023-08-30 17:10:25 +0700

    Csharp 5

---
+++ WhatNewCSharp5/AsyncFeature/Product.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+
+namespace AsyncFeature
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public int price { get; set; }
+        public float discountPercentage { get; set; }
+        public float rating { get; set; }
+        public int stock { get; set; }
+        public string brand { get; set; }
+        public string category { get; set; }
+        public string thumbnail { get; set; }
+        public string[] images { get; set; }
+
+        public class Rootobject
+        {
+            public Product[] products { get; set; }
+            public int total { get; set; }
+            public int skip { get; set; }
+            public int limit { get; set; }
+        }
+
+        public static async Task<Rootobject> GetProductList()
+        {
+            HttpClient client = new HttpClient();
+            Uri address = new Uri("https://dummyjson.com/");
+            client.BaseAddress = address;
+
+            HttpResponseMessage response = await client.GetAsync("products");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var list = await response.Content.ReadAsStringAsync();
+                if (!string.IsNullOrEmpty(list))
+                {
+                    return JsonConvert.DeserializeObject<Rootobject>(list);
+                }
+                return null;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ WhatNewCSharp5/AsyncFeature/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AsyncFeature
+{
+    internal class Program
+    {
+
+        public static IEnumerable<Product> GetProduct()
+        {
+            return Product.GetProductList().Result.products;
+        }
+
+        static void Main(string[] args)
+        {
+
+            var list = GetProduct();
+            if (list != null)
+            {
+                foreach (var item in list)
+                {
+                    Console.Write($"Id: {item.id}   ");
+                    Console.Write($"Title: {item.title}   ");
+                    Console.Write($"Description: {item.description}   ");
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
